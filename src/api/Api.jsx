a9ed38cd9d8f04cd59606f035929805548a5d123
noneofurbuzz <ugochiike26@gmail.com>
2023-09-15 13:42:49 +0100

    Fix: hosting issue

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -2,6 +2,6 @@ export const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: `Bearer ${import.meta.env.VITE_API_KEY}.eyJhdWQiOiIzZGJhMDgzODlkOWM5NzdiOTRjM2RiMmQxZDRlNWYzOCIsInN1YiI6IjY0ZmU0NTAxZmZjOWRlMGVlM2M0YzJlZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z-rZK7imtrGnlWaGdNJUDrtD6UtyOfZG_CGGeaQ3pF8`
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZGJhMDgzODlkOWM5NzdiOTRjM2RiMmQxZDRlNWYzOCIsInN1YiI6IjY0ZmU0NTAxZmZjOWRlMGVlM2M0YzJlZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z-rZK7imtrGnlWaGdNJUDrtD6UtyOfZG_CGGeaQ3pF8`
     }
   };
